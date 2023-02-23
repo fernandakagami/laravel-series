782b1eaaca2b420f1310232e85cc9e6b2d73c30c
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-23 09:42:35 -0300

    created store controller - stored new series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,16 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'House Of Dragon'
-        ];
+        $series = DB::select('SELECT nome FROM series;');
 
         return view('series.index', [
             'series' => $series
@@ -22,5 +20,16 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+
+        if (DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
+            return "ok";
+        } else {
+            return "Deu Erro";
+        }
     }
 }
