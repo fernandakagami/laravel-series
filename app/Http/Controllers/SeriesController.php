5bd61f2612f31997b3935f1cd552d080d886c9aa
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-24 10:33:07 -0300

    created edit/update , created validation

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Serie;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $series = Serie::all();
+        $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');        
 
-        return view('series.index', [
-            'series' => $series
-        ]);
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create()
@@ -25,8 +22,29 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
-        Serie::create($request->all());
+        $request->validate([
+            'nome' => ['required', 'min:3']
+        ]);
+        $serie = Serie::create($request->all());
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
 
-        return to_route('series.index');
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso.");
+    }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request)
+    {
+        $series->nome = $request->nome;
+        $series->save();
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' editada com sucesso.");
     }
 }
