59b6a856ff5323112724285fa8e69b1cfc433614
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-24 16:43:34 -0300

    updated controllers and views

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Models\Season;
+use App\Models\Episode;
 
 class SeriesController extends Controller
 {
@@ -23,17 +25,25 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $serie = Series::create($request->all());
+        $seasons = [];
         for ($i = 1; $i <= $request->seasonsQty; $i++) {
-            $season = $serie->seasons()->create([
+            $seasons[] = [
+                'series_id' => $serie->id,
                 'number' => $i
-            ]);
+            ];
+        };
+        Season::insert($seasons);
 
+        $episodes = [];
+        foreach ($serie->seasons as $season) {            
             for($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                $season->episodes()->create([
+                $episodes[] = [
+                    'season_id' => $season->id,
                     'number' => $j
-                ]);
-            }            
-        }
+                ];
+            }       
+        };
+        Episode::insert($episodes);
 
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
