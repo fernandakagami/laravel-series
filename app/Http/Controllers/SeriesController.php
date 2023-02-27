afa2bfc8d76cfa66a22c8cce83d59ce3c9e80a3e
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-27 08:47:50 -0300

    created repository

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,10 +6,16 @@ use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
 use App\Models\Season;
 use App\Models\Episode;
+use App\Repositories\SeriesRepository;
 use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $repository)
+    {
+
+    }
+
     public function index()
     {
         $series = Series::with(['seasons'])->get();
@@ -25,30 +31,7 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {     
-        $serie = DB::transaction(function() use ($request) {
-            $serie = Series::create($request->all());
-            $seasons = [];
-            for ($i = 1; $i <= $request->seasonsQty; $i++) {
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i
-                ];
-            };
-            Season::insert($seasons);
-    
-            $episodes = [];
-            foreach ($serie->seasons as $season) {            
-                for($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                }       
-            };
-            Episode::insert($episodes);
-            
-            return $serie;            
-        }); 
+        $serie = $this->repository->add($request);
 
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso"); 
     }
