b21b36c9172b961909904853d38b1a7b3871c123
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-27 09:03:15 -0300

    created provider

---
+++ app/Providers/SeriesRepositoryProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\EloquentSeriesRepository;
+use App\Repositories\SeriesRepository;
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Support\ServiceProvider;
+
+class SeriesRepositoryProvider extends ServiceProvider
+{
+    public array $bindings = [
+        SeriesRepository::class => EloquentUserProvider::class
+    ];
+}
