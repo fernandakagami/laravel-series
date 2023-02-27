bf5628f7843669064518c66f820b56acb60a1e2b
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-02-27 08:04:54 -0300

    Update SeriesController.php

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
 use App\Models\Season;
 use App\Models\Episode;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
@@ -23,29 +24,33 @@ class SeriesController extends Controller
     }
 
     public function store(SeriesFormRequest $request)
-    {
-        $serie = Series::create($request->all());
-        $seasons = [];
-        for ($i = 1; $i <= $request->seasonsQty; $i++) {
-            $seasons[] = [
-                'series_id' => $serie->id,
-                'number' => $i
-            ];
-        };
-        Season::insert($seasons);
-
-        $episodes = [];
-        foreach ($serie->seasons as $season) {            
-            for($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                $episodes[] = [
-                    'season_id' => $season->id,
-                    'number' => $j
+    {     
+        $serie = DB::transaction(function() use ($request) {
+            $serie = Series::create($request->all());
+            $seasons = [];
+            for ($i = 1; $i <= $request->seasonsQty; $i++) {
+                $seasons[] = [
+                    'series_id' => $serie->id,
+                    'number' => $i
                 ];
-            }       
-        };
-        Episode::insert($episodes);
+            };
+            Season::insert($seasons);
+    
+            $episodes = [];
+            foreach ($serie->seasons as $season) {            
+                for($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j
+                    ];
+                }       
+            };
+            Episode::insert($episodes);
+            
+            return $serie;            
+        }); 
 
-        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso"); 
     }
 
     public function destroy(Series $series)
